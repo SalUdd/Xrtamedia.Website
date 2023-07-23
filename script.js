7d2172f577d78fcd81a1e3538d4ef2021497306b
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2023-07-23 17:23:18 +0800

    Changes to initial Files

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $("nav ul.menu > li").hover(function() {
-      $(this).find("ul.submenu").stop().slideToggle(300);
-    });
+  $("nav ul.menu > li, nav ul.submenu > li").hover(function() {
+    $(this).children("ul.submenu").stop().slideToggle(300);
   });
-  
+});
