34ab85eaa2d48fb35b3739e12ed1f26d73e7bbb8
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2023-07-23 20:41:34 +0800

    added hover and timer to retract hover

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $("nav ul.menu > li, nav ul.submenu > li").hover(function() {
-    $(this).children("ul.submenu").stop().slideToggle(300);
+  var timer;
+  $("nav ul.menu > li").hover(function() {
+    clearTimeout(timer);
+    $(this).children("ul.submenu").stop(true, true).slideDown(300);
+  }, function() {
+    var that = $(this);
+    timer = setTimeout(function() {
+      that.children("ul.submenu").stop(true, true).slideUp(300);
+    }, 150); // 0.15 second delay before the dropdown disappears
   });
 });
